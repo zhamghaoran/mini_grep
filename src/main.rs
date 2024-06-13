c1627c2e9b5f1feac09e5b16fe08e5a96a6d3b03
zhanghaoran.void <zhanghaoran.void@bytedance.com>
2024-06-14 00:51:01 +0800

    "内容输入"

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("query:{:?}", query);
+    println!("file path:{:?}", file_path);
+}
